32e0dd742d76d6f53b1446c021fd4851ba803345
Volcomix <Volcomix@users.noreply.github.com>
2026-05-26 16:50:47 +0200

    Bootstrap advertising

--- main/ble.c
+++ main/ble.c
@@ -10,6 +10,37 @@ static const char *tag = "volcomix_ble";
 static void on_sync(void)
 {
     ESP_LOGI(tag, "BLE stack synced");
+
+    int rc;
+    struct ble_hs_adv_fields fields = {0};
+    struct ble_gap_adv_params adv_params = {0};
+    const char *name;
+
+    fields.flags = BLE_HS_ADV_F_DISC_GEN |
+                   BLE_HS_ADV_F_BREDR_UNSUP;
+
+    fields.tx_pwr_lvl_is_present = 1;
+    fields.tx_pwr_lvl = BLE_HS_ADV_TX_PWR_LVL_AUTO;
+
+    name = ble_svc_gap_device_name();
+    fields.name = (uint8_t *)name;
+    fields.name_len = strlen(name);
+    fields.name_is_complete = 1;
+
+    rc = ble_gap_adv_set_fields(&fields);
+    if (rc != 0) {
+        MODLOG_DFLT(ERROR, "error setting advertisement data; rc=%d\n", rc);
+        return;
+    }
+
+    adv_params.conn_mode = BLE_GAP_CONN_MODE_UND;
+    adv_params.disc_mode = BLE_GAP_DISC_MODE_GEN;
+    rc = ble_gap_adv_start(BLE_OWN_ADDR_PUBLIC, NULL, BLE_HS_FOREVER,
+                           &adv_params, NULL, NULL);
+    if (rc != 0) {
+        MODLOG_DFLT(ERROR, "error enabling advertisement; rc=%d\n", rc);
+        return;
+    }
 }
 
 void host_task(void *param)
@@ -40,7 +71,7 @@ void app_main(void)
 
     ble_svc_gap_init();
 
-    rc = ble_svc_gap_device_name_set("volcomix-esp32");
+    rc = ble_svc_gap_device_name_set("Volcomix ESP32");
     assert(rc == 0);
 
     nimble_port_freertos_init(host_task);
