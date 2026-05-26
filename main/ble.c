4480f757915d1cd285f8c09563591a63240274ee
Volcomix <Volcomix@users.noreply.github.com>
2026-05-26 15:46:19 +0200

    Bootstrap BLE

--- main/ble.c
+++ main/ble.c
@@ -1,11 +1,28 @@
 #include "esp_log.h"
 #include "nvs_flash.h"
 #include "nimble/nimble_port.h"
+#include "nimble/nimble_port_freertos.h"
+#include "host/ble_hs.h"
+#include "services/gap/ble_svc_gap.h"
 
 static const char *tag = "volcomix_ble";
 
+static void on_sync(void)
+{
+    ESP_LOGI(tag, "BLE stack synced");
+}
+
+void host_task(void *param)
+{
+    ESP_LOGI(tag, "BLE host task started");
+    nimble_port_run();
+    nimble_port_freertos_deinit();
+}
+
 void app_main(void)
 {
+    int rc;
+
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
         ESP_ERROR_CHECK(nvs_flash_erase());
@@ -19,5 +36,12 @@ void app_main(void)
         return;
     }
 
-    ESP_LOGI(tag, "Running...");
+    ble_hs_cfg.sync_cb = on_sync;
+
+    ble_svc_gap_init();
+
+    rc = ble_svc_gap_device_name_set("volcomix-esp32");
+    assert(rc == 0);
+
+    nimble_port_freertos_init(host_task);
 }
