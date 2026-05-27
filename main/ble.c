8baaff80cdd22ab36690b030a5f92da9eb8fc5f7
Volcomix <Volcomix@users.noreply.github.com>
2026-05-27 12:36:25 +0200

    Handle writing characteristic

--- main/ble.c
+++ main/ble.c
@@ -4,43 +4,114 @@
 #include "nimble/nimble_port_freertos.h"
 #include "host/ble_hs.h"
 #include "services/gap/ble_svc_gap.h"
+#include "services/gatt/ble_svc_gatt.h"
 
 static const char *tag = "volcomix_ble";
+
+// 11724e4b-670a-c399-fe43-4d4a4a281800
+static const ble_uuid128_t service_uuid =
+    BLE_UUID128_INIT(
+        0x00, 0x18, 0x28, 0x4a, 0x4a, 0x4d,
+        0x43, 0xfe,
+        0x99, 0xc3,
+        0x0a, 0x67,
+        0x4b, 0x4e, 0x72, 0x11);
+
+// 11724e4b-670a-c399-fe43-4d4a4a281801
+static const ble_uuid128_t char_uuid =
+    BLE_UUID128_INIT(
+        0x01, 0x18, 0x28, 0x4a, 0x4a, 0x4d,
+        0x43, 0xfe,
+        0x99, 0xc3,
+        0x0a, 0x67,
+        0x4b, 0x4e, 0x72, 0x11);
+
+static int char_access(uint16_t conn_handle, uint16_t attr_handle,
+                       struct ble_gatt_access_ctxt *ctxt, void *arg)
+{
+    ESP_LOGI(tag, "Characteristic written %d", ctxt->om->om_data[0]);
+    return 0;
+}
+
+static const struct ble_gatt_svc_def gatt_svr_svcs[] = {
+    {.type = BLE_GATT_SVC_TYPE_PRIMARY,
+     .uuid = &service_uuid.u,
+     .characteristics = (struct ble_gatt_chr_def[]){
+         {.uuid = &char_uuid.u,
+          .access_cb = char_access,
+          .flags = BLE_GATT_CHR_F_WRITE_NO_RSP},
+         {0},
+     }},
+    {0},
+};
+
+static int on_gap_event(struct ble_gap_event *event, void *arg)
+{
+    switch (event->type)
+    {
+    case BLE_GAP_EVENT_CONNECT:
+        if (event->connect.status == 0)
+        {
+            ESP_LOGI(tag, "Connection established");
+        }
+        else
+        {
+            ESP_LOGE(tag, "Connection failed; status=%d", event->connect.status);
+        }
+        break;
+    case BLE_GAP_EVENT_DISCONNECT:
+        ESP_LOGI(tag, "Disconnected; reason=%d", event->disconnect.reason);
+        break;
+    }
+    return 0;
+}
 
 static void on_sync(void)
 {
     ESP_LOGI(tag, "BLE stack synced");
 
     int rc;
-    struct ble_hs_adv_fields fields = {0};
+    struct ble_hs_adv_fields adv_fields = {0};
+    struct ble_hs_adv_fields rsp_fields = {0};
     struct ble_gap_adv_params adv_params = {0};
     const char *name;
 
-    fields.flags = BLE_HS_ADV_F_DISC_GEN |
-                   BLE_HS_ADV_F_BREDR_UNSUP;
+    adv_fields.flags = BLE_HS_ADV_F_DISC_GEN |
+                       BLE_HS_ADV_F_BREDR_UNSUP;
 
-    fields.tx_pwr_lvl_is_present = 1;
-    fields.tx_pwr_lvl = BLE_HS_ADV_TX_PWR_LVL_AUTO;
+    adv_fields.tx_pwr_lvl_is_present = 1;
+    adv_fields.tx_pwr_lvl = BLE_HS_ADV_TX_PWR_LVL_AUTO;
 
     name = ble_svc_gap_device_name();
-    fields.name = (uint8_t *)name;
-    fields.name_len = strlen(name);
-    fields.name_is_complete = 1;
+    adv_fields.name = (uint8_t *)name;
+    adv_fields.name_len = strlen(name);
+    adv_fields.name_is_complete = 1;
 
-    rc = ble_gap_adv_set_fields(&fields);
+    rc = ble_gap_adv_set_fields(&adv_fields);
     if (rc != 0)
     {
-        MODLOG_DFLT(ERROR, "error setting advertisement data; rc=%d\n", rc);
+        MODLOG_DFLT(ERROR, "Error setting advertisement data; rc=%d\n", rc);
+        return;
+    }
+
+    rsp_fields.uuids128 = (ble_uuid128_t[]){service_uuid};
+    rsp_fields.num_uuids128 = 1;
+    rsp_fields.uuids128_is_complete = 1;
+
+    rc = ble_gap_adv_rsp_set_fields(&rsp_fields);
+    if (rc != 0)
+    {
+        MODLOG_DFLT(ERROR, "Error setting scan response data; rc=%d\n", rc);
         return;
     }
 
     adv_params.conn_mode = BLE_GAP_CONN_MODE_UND;
     adv_params.disc_mode = BLE_GAP_DISC_MODE_GEN;
     rc = ble_gap_adv_start(BLE_OWN_ADDR_PUBLIC, NULL, BLE_HS_FOREVER,
-                           &adv_params, NULL, NULL);
+                           &adv_params, on_gap_event, NULL);
     if (rc != 0)
     {
-        MODLOG_DFLT(ERROR, "error enabling advertisement; rc=%d\n", rc);
+        MODLOG_DFLT(ERROR, "Error enabling advertisement; rc=%d\n", rc);
         return;
     }
 }
@@ -74,6 +145,14 @@ void app_main(void)
     ble_hs_cfg.sync_cb = on_sync;
 
     ble_svc_gap_init();
+
+    ble_svc_gatt_init();
+
+    rc = ble_gatts_count_cfg(gatt_svr_svcs);
+    assert(rc == 0);
+
+    rc = ble_gatts_add_svcs(gatt_svr_svcs);
+    assert(rc == 0);
 
     rc = ble_svc_gap_device_name_set("Volcomix ESP32");
     assert(rc == 0);
